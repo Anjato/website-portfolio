2fa14068a8fb35abc9adb26111593ce81a80f385
Andrew <atolkinen@gmail.com>
2021-11-14 07:58:23 -0600

    completed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import {AiFillGithub, AiFillInstagram, AiFillLinkedin, AiFillYoutube} from 'react-icons/ai';
 
 import { SocialIcons } from '../Header/HeaderStyles';
 import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+        <LinkList>
+            <LinkColumn>
+                <LinkTitle>Email</LinkTitle>
+                <LinkItem href={"mailto:atolkinen@gmail.com"} target={"__blank"}>atolkinen@gmail.com</LinkItem>
+            </LinkColumn>
+        </LinkList>
+        <SocialIconsContainer>
+            <CompanyContainer>
+                <Slogan>Feel free to reach out for any inquiries.</Slogan>
+            </CompanyContainer>
+            <SocialIcons href="https://www.github.com/Anjato" target="_blank">
+                <AiFillGithub size="3rem" />
+            </SocialIcons><SocialIcons href="https://www.linkedin.com/in/andrew-t-704a161a4" target="_blank">
+            <AiFillLinkedin size="3rem" />
+        </SocialIcons>
+            <SocialIcons href="https://www.youtube.com/channel/UC8HHtru-c-h8OJao60JdnEg" target="_blank">
+                <AiFillYoutube size="3rem" />
+            </SocialIcons>
+        </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
