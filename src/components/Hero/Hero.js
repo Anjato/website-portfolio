fcf97eed248d6c3f8c82901e4adce94bda61eb87
Andrew <atolkinen@gmail.com>
2021-11-14 04:27:22 -0600

    "Welcome" section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+      <LeftSection>
+          <SectionTitle main center>
+              Welcome to <br />
+              My Portfolio!
+          </SectionTitle>
+          <SectionText>
+              The purpose of my portfolio is to showcase of some of my projects and provide a short description about who I am.
+          </SectionText>
+          <Button onClick={() => window.location = "#about"}>Contact Me</Button>
+      </LeftSection>
+  </Section>
 );
 
 export default Hero;
