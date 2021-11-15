078f9dfbe70ae8e88ae3234e00581ade4e629d57
Andrew <atolkinen@gmail.com>
2021-11-15 02:32:39 -0600

    formatting changes

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -1,7 +1,20 @@
 import React, { useState, useRef, useEffect } from 'react';
 
-import { CarouselButton, CarouselButtonDot, CarouselButtons, CarouselContainer, CarouselItem, CarouselItemImg, CarouselItemText, CarouselItemTitle, CarouselMobileScrollNode } from './TimeLineStyles';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import { CarouselButton,
+    CarouselButtonDot,
+    CarouselButtons,
+    CarouselContainer,
+    CarouselItem,
+    CarouselItemImg,
+    CarouselItemText,
+    CarouselItemTitle,
+    CarouselMobileScrollNode
+} from './TimeLineStyles';
+import { Section,
+    SectionDivider,
+    SectionText,
+    SectionTitle
+} from '../../styles/GlobalComponents';
 import { TimeLineData } from '../../constants/constants';
 
 const TOTAL_CAROUSEL_COUNT = TimeLineData.length;
@@ -26,7 +39,10 @@ const Timeline = () => {
 
   const handleScroll = () => {
     if (carouselRef.current) {
-      const index = Math.round((carouselRef.current.scrollLeft / (carouselRef.current.scrollWidth * 0.7)) * TimeLineData.length);
+      const index = Math.round(
+          (carouselRef.current.scrollLeft /
+              (carouselRef.current.scrollWidth * 0.7)) *
+          TimeLineData.length);
 
       setActiveItem(index);
     }
