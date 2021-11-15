3e7df6ce4a06fdc97cc7c75f6d6bf3c3dda371e4
Andrew <atolkinen@gmail.com>
2021-11-15 04:49:57 -0600

    added new projects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,7 @@
 export const projects = [
   {
     title: 'Website Portfolio',
-    description: "This was my first website developed using React.JS and Next.JS. I created this as a visually appealing supplement to my resume and to begin hands on learning with full stack web development.",
+    description: "This was my first website developed using React.js and Next.js. I created this as a visually appealing supplement to my resume and to begin hands on learning with full stack web development.",
       image: '/images/1.png',
       tags: ['React', 'Next', 'Node'],
     source: '/',
@@ -9,30 +9,37 @@ export const projects = [
     id: 0,
   },
   {
-    title: 'Project Duck',
-    description:"This is just filler text for the project description. I currently do not have a project posted here but will be changed as I keep working on this website :)",
+    title: 'MERN Stack Website',
+    description:"I created this website in preparation for more in-depth learning of the MERN stack through Michigan State's boot camp. Before the boot camp started, I wanted to learn as much full stack web development on my own that I could.",
     image: '/images/tbd.jpg',
-    tags: ['Tag', "You're", 'It', ':)'],
+    tags: ['MongoDB', 'Express', 'React', 'Node'],
     source: 'https://google.com',
     code: 'https://google.com',
     id: 1,
   },
   {
-    title: 'Project Truck',
-    description: "This is just filler text for the project description. I currently do not have a project posted here but will be changed as I keep working on this website :)",
-      image: '/images/tbd.jpg',
-      tags: ['Tags', 'Here'],
+    title: 'C++ Projects',
+    description: "This is a list of all of my C++ projects that I included in a single repository for easier access " +
+        "than uploading several different repositories. These projects range from introductory content to other projects" +
+        " I learned to make on my own from scratch from 2017-2021. More than likely the rest of my projects will be" +
+        "uploaded through separate repositories because I plan on uploading only large projects in the future.",
+      image: '/images/cpp.jpg',
+      tags: ['C++', 'C'],
     source: 'https://google.com',
-    code: 'https://google.com',
+    code: 'https://github.com/Anjato/CPP-Portfolio',
     id: 2,
   },
   {
-    title: 'Project Stuck',
-    description: "This is just filler text for the project description. I currently do not have a project posted here but will be changed as I keep working on this website :)",
-    image: '/images/tbd.jpg',
-    tags: ['Im', 'A', 'Tag'],
+    title: 'Python Projects',
+    description: "This is a list of all my Python projects that I included in a single repository. I decided to have a" +
+        "similar repository layout as with the C++ repository but will start to upload only larger projects. The" +
+        "type racer project will eventually be moved to its own repository as I plan on expanding to include" +
+        "signing into an account and image captcha recognition in order to have your typing speed automatically" +
+        "verified.",
+    image: '/images/python.jpg',
+    tags: ['Python'],
     source: 'https://google.com',
-    code: 'https://google.com',
+    code: 'https://github.com/Anjato/Python-Portfolio',
     id: 3,
   },
 ];
