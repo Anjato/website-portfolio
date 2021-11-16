e09de1d199048a5ad67e26905e60a98e6331f60f
Andrew <atolkinen@gmail.com>
2021-11-15 19:34:07 -0600

    updated tab name

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,6 @@ import Technologies from '../components/Technologies/Technologies';
 import Timeline from '../components/TimeLine/TimeLine';
 import { Layout } from '../layout/Layout';
 import { Section } from '../styles/GlobalComponents';
-import { useEffect } from "react";
 
 const Home = () => {
   return (
