be6a91d0d3e42297e66b4c7752edcc202c6130a5
Andrew <atolkinen@gmail.com>
2022-02-03 23:49:54 -0600

    fixed typos

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -9,8 +9,8 @@ export const projects = [
     id: 0,
   },
   {
-    title: 'MERN Stack Website',
-    description:"I created this website in preparation for more in-depth learning of the MERN stack through Michigan State's boot camp. Before the boot camp started, I wanted to learn as much full stack web development on my own that I could.",
+    title: 'MERN Stack Website [TBD]',
+    description:"TBD; some ideas planned!",
     image: '/images/mern.jpg',
     tags: ['MongoDB', 'Express', 'React', 'Node'],
     source: 'https://google.com',
@@ -20,8 +20,8 @@ export const projects = [
   {
     title: 'C++ Projects',
     description: "This is a list of all of my C++ projects that I included in a single repository for easier access " +
-        "than uploading several different repositories. These projects range from introductory content to other projects" +
-        " I learned to make on my own from scratch from 2017-2021. More than likely the rest of my projects will be" +
+        "than uploading several different repositories. These projects range from introductory content to other projects " +
+        " I learned to make on my own from scratch from 2017-2021. More than likely the rest of my projects will be " +
         "uploaded through separate repositories because I plan on uploading only large projects in the future.",
       image: '/images/cpp.png',
       tags: ['C++', 'C'],
@@ -31,10 +31,10 @@ export const projects = [
   },
   {
     title: 'Python Projects',
-    description: "This is a list of all my Python projects that I included in a single repository. I decided to have a" +
-        "similar repository layout as with the C++ repository but will start to upload only larger projects. The" +
-        "type racer project will eventually be moved to its own repository as I plan on expanding to include" +
-        "signing into an account and image captcha recognition in order to have your typing speed automatically" +
+    description: "This is a list of all my Python projects that I included in a single repository. I decided to have a " +
+        "similar repository layout as with the C++ repository but will start to upload only larger projects. The " +
+        "type racer project will eventually be moved to its own repository as I plan on expanding to include " +
+        "signing into an account and image captcha recognition in order to have your typing speed automatically " +
         "verified.",
     image: '/images/python.jpg',
     tags: ['Python'],
@@ -49,6 +49,6 @@ export const TimeLineData = [
   { year: 2015, text: 'Took a break and shifted my career to focus on another path', },
   { year: 2017, text: 'Began learning C++, Java, and basic Assembly', },
   { year: 2020, text: 'Hired with Target. Began learning Python', },
-  { year: 2021, text: 'Accepted into MSU boot camp for Jan - Jul 2022. Began learning full stack web development ahead of boot camp', },
+  { year: 2021, text: 'Accepted into MSU boot camp for Jan - Jul 2022. Created first mod for Subnautica.', },
   { year: 2022, text: 'Started MSU Bootcamp. Began learning Java & Kotlin to create OpenOSRS plugins.'},
 ];
