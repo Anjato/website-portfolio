75fc65dddc87b68f854aa3a0dacbe208458f16a8
Anjato <atolkinen@gmail.com>
2022-08-24 01:29:01 -0500

    added homelab

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Hero from '../components/Hero/Hero';
 import Projects from '../components/Projects/Projects';
 import Technologies from '../components/Technologies/Technologies';
 import Timeline from '../components/TimeLine/TimeLine';
+import HomeLab from '../components/HomeLabInfo/HomeLab'
 import { Layout } from '../layout/Layout';
 import { Section } from '../styles/GlobalComponents';
 
@@ -16,6 +17,7 @@ const Home = () => {
       </Section>
       <Projects />
       <Technologies />
+      <HomeLab />
       <Timeline />
       <Acomplishments />
     </Layout>
